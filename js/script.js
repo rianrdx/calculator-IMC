4e9310148e9658fc6e3df9ffcb5dbd98c8ee9890
Rian Guedes <rianrdx0@gmail.com>
2023-09-26 19:27:49 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+import { Modal } from "./modal.js";
+import { AlertError } from "./alert-error.js";
+import { notNumber, calculateIMC } from "./utils.js";
+
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
+
+inputHeight.oninput = () => AlertError.close();
+inputWeight.oninput = () => AlertError.close();
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+  const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height);
+
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open();
+    return;
+  }
+
+  AlertError.close();
+
+  const result = calculateIMC(weight, height);
+  displayResultMessage(result);
+};
+
+function displayResultMessage(result) {
+  Modal.message.innerText = result;
+  Modal.open();
+}
